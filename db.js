5f4ccdbcc924777c9d4830b02115f0fadec18519
CesarG503 <cesargaray06@gmail.com>
2025-05-16 11:19:52 -0600

    variables de entorno y modificacion de la base de datos

--- db.js
+++ db.js
@@ -1,11 +1,18 @@
 const { Pool } = require('pg');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const pool = new Pool({
-  host: 'localhost',
-  user: 'postgres',
-  password: 'admin', // Ingresa tu contraseña de PostgreSQL (cambiala loco)
-  database: 'PokemonDB',
-  port: 5432
+
+  //host: process.env.LOCAL,
+  
+  //user: 'postgres',
+  //password: 'admin', // Ingresa tu contraseña de PostgreSQL (cambiala loco)
+  //database: 'PokemonDB',
+  //port: 5432
+  connectionString: process.env.DATA_BASE_URL,
+  ssl: true
 });
 
 pool.connect((err) => {
